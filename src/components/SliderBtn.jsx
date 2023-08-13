c6bdbab9ca1397155b30a98da968396054d93c81
daniel <daniel.l.babin@gmail.com>
2023-08-13 15:06:49 -0400

    fixed button

--- src/components/SliderBtn.jsx
+++ src/components/SliderBtn.jsx
@@ -1,13 +1,34 @@
-function SliderBtn({ name }) {
+function SliderBtn({ name, count, setCount, listSize }) {
   let delay = (time) => {
     return new Promise((res) => {
       setTimeout(res, time)
     })
   }
 
+  const prev = async (e) => {
+    if (count <= 0) {
+      setCount(listSize - 1)
+    } else {
+      setCount(count - 1)
+    }
+    e.target.disabled = true
+    await delay(600)
+    e.target.disabled = false
+  }
+
+  const next = async (e) => {
+    if (count >= listSize - 1) {
+      setCount(0)
+    } else {
+      setCount(count + 1)
+    }
+    e.target.disabled = true
+    await delay(600)
+    e.target.disabled = false
+  }
   return (
     <>
-      <button id={name} onClick={eval(name)}>
+      <button id={name} onClick={{ name }()}>
         {name}
       </button>
     </>
